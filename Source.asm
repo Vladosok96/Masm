8e2b1d757b47ce705b85d02a5784bc85bb1a59e4
Vladosok96 <vmixoks@gmail.com>
2023-05-14 17:43:08 +0400

    Final

--- Source.asm
+++ Source.asm
@@ -14,9 +14,10 @@
 	ControllerInfo		db "KR1802VS1 (F0 = 1)", 10, 13, 0
 	DAPrompt   			db "Enter DA(0-7) >>>", 0
 	DBPrompt			db "Enter DB(0-7) >>>", 0
-	inputFPrompt		db "Enter F(0-7) >>>", 0
+	FPrompt				db "Enter F(0-7) >>>", 0
+	CIPrompt			db "Enter CI >>>", 0
 
-	print8String		db "%8s", 0
+	scanf8String		db "%8s", 0
 	scanInteger			db "%d", 0
 	printInteger   		db "%d ", 10, 13, 0
 	printFloat			db "%f ", 0
@@ -24,18 +25,26 @@
 
 	DAInput				db "00000000", 0
 	DBInput				db "00000000", 0
+	FInput				db "00000000", 0
+	CIInput				db "00000000", 0
 	DAbyte				db 0
 	DBbyte				db 0
 	Fbyte				db 0
+	CIbyte				db 0
+
+	DAOutput			db "00000000", 10, 13, 0
+	DBOutput			db "00000000", 10, 13, 0
+	DAResult   			db "Enter DA(0-7) = ", 0
+	DBResult			db "Enter DB(0-7) = ", 0
 
 .code
 
 start:
 
-	invoke		crt_printf, offset ControllerInfo, 0	
+	invoke		crt_printf, offset ControllerInfo, 0
 	
-	invoke		crt_printf, offset DAPrompt, 0	
-	invoke		crt_scanf, offset print8String, offset DAInput
+	invoke		crt_printf, offset DAPrompt, 0
+	invoke		crt_scanf, offset scanf8String, offset DAInput
 	
 	mov ebx, 0
 	mov esi, offset DAInput
@@ -51,8 +60,8 @@ DA_next:
 	mov DAbyte, bl
 
 
-	invoke		crt_printf, offset DBPrompt, 0	
-	invoke		crt_scanf, offset print8String, offset DBInput
+	invoke		crt_printf, offset DBPrompt, 0
+	invoke		crt_scanf, offset scanf8String, offset DBInput
 
 	mov ebx, 0
 	mov esi, offset DBInput
@@ -67,11 +76,211 @@ DB_next:
     loop DB_loop_start
 	mov DBbyte, bl
 
-	mov eax, 0
+	invoke		crt_printf, offset FPrompt, 0
+	invoke		crt_scanf, offset scanf8String, offset FInput
+
 	mov ebx, 0
-	mov al, DAbyte
+	mov esi, offset FInput
+    mov ecx, 8
+F_loop_start:
+	shl ebx, 1
+    lodsb
+	cmp al, 31h
+	jne F_next
+	inc ebx
+F_next:
+    loop F_loop_start
+	mov Fbyte, bl
+
+	invoke		crt_printf, offset CIPrompt, 0
+	invoke		crt_scanf, offset scanf8String, offset CIInput
+
+	mov esi, offset CIInput
+    lodsb
+	cmp al, 31h
+	jne Start_compute
+	mov CIbyte, 1
+
+
+Start_compute:
+	mov ebx, 0
+	mov ecx, 0
+	mov bl, DAbyte
+	mov cl, DBbyte
+
+	mov al, Fbyte
+	bt ax, 7		; Test F0
+	jnc End_compute
+	
+Test_F_1000:		; Test F=1000
+	
+	bt ax, 6		; F1=0
+	jc Test_F_1001
+
+	bt ax, 5		; F2=0
+	jc Test_F_1001
+
+	bt ax, 4		; F3=0
+	jc Test_F_1001
+
+	; Inversion B
+	xor cl, 11111111b
+	add cl, CIbyte
+	mov DBbyte, cl
+	jmp End_compute
+
+Test_F_1001:		; Test F=1001
+	
+	bt ax, 6		; F1=0
+	jc Test_F_1010
+
+	bt ax, 5		; F2=0
+	jc Test_F_1010
+
+	bt ax, 4		; F3=1
+	jnc Test_F_1010
+
+	; Restriction by B
+	xor cl, 11111111b
+	and bl, cl
+	mov DAbyte, bl
+	mov DBbyte, cl
+
+Test_F_1010:		; Test F=1010
+	
+	bt ax, 6		; F1=0
+	jc Test_F_1011
+
+	bt ax, 5		; F2=1
+	jnc Test_F_1011
+
+	bt ax, 4		; F3=0
+	jc Test_F_1011
+
+	; Sending A
+	add bl, CIbyte
+	mov DAbyte, bl
+
+Test_F_1011:		; Test F=1011
+	
+	bt ax, 6		; F1=0
+	jc Test_F_1100
+
+	bt ax, 5		; F2=1
+	jnc Test_F_1100
+
+	bt ax, 4		; F3=1
+	jnc Test_F_1100
+
+	; Disjunction
+	or bl, cl
+	mov DAbyte, bl
+	mov DBbyte, cl
+
+Test_F_1100:		; Test F=1100
+	
+	bt ax, 6		; F1=1
+	jnc Test_F_1101
+
+	bt ax, 5		; F2=0
+	jc Test_F_1101
+
+	bt ax, 4		; F3=0
+	jc Test_F_1101
+
+	; Fields substraction
+	sub bl, cl
+	sub bl, 1
+	add bl, CIbyte
+	mov DAbyte, bl
+	mov DBbyte, cl
+
+Test_F_1101:		; Test F=1101
+	
+	bt ax, 6		; F1=1
+	jnc Test_F_1110
+
+	bt ax, 5		; F2=0
+	jc Test_F_1110
+
+	bt ax, 4		; F3=1
+	jnc Test_F_1110
+
+	; XOR
+	xor bl, cl
+	mov DAbyte, bl
+	mov DBbyte, cl
+
+Test_F_1110:		; Test F=1110
+	
+	bt ax, 6		; F1=1
+	jnc Test_F_1111
+
+	bt ax, 5		; F2=1
+	jnc Test_F_1111
+
+	bt ax, 4		; F3=0
+	jc Test_F_1111
+
+	; Sending B
+	add cl, CIbyte
+	mov DBbyte, cl
+
+Test_F_1111:		; Test F=1111
+	
+	bt ax, 6		; F1=1
+	jnc End_compute
+
+	bt ax, 5		; F2=1
+	jnc End_compute
+
+	bt ax, 4		; F3=1
+	jnc End_compute
+
+	; Reverse fields substraction
+	sub cl, bl
+	sub cl, 1
+	add cl, CIbyte
+	mov DAbyte, bl
+	mov DBbyte, cl
+
+End_compute:
+
+	mov bl, DAbyte
+	mov esi, offset DAOutput
+	add esi, 7
+    mov ecx, 0
+DA_loop_print:
+	bt bx, 0
+	jnc DA_loop_print_zero
+	mov byte ptr [esi], 31h
+DA_loop_print_zero:
+	shr bx, 1
+	dec esi
+	inc ecx
+    cmp ecx, 8
+	jl DA_loop_print
+
+	invoke		crt_printf, offset DAResult, 0
+	invoke		crt_printf, offset DAOutput, 0
+	
 	mov bl, DBbyte
-	add al, bl
+	mov esi, offset DBOutput
+	add esi, 7
+    mov ecx, 0
+DB_loop_print:
+	bt bx, 0
+	jnc DB_loop_print_zero
+	mov byte ptr [esi], 31h
+DB_loop_print_zero:
+	shr bx, 1
+	dec esi
+	inc ecx
+    cmp ecx, 8
+	jl DB_loop_print
+
+	invoke		crt_printf, offset DBResult, 0
+	invoke		crt_printf, offset DBOutput, 0
 
 	push 0
 	call ExitProcess
